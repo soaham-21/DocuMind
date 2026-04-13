3050af76fdfd815a078b20a1f1c3875217a139da
Soaham Pahwa <soaham221@gmail.com>
2026-04-13 05:39:10 +0530

    retriever

--- OneDrive/Desktop/smart research assistant/retriever.py
+++ OneDrive/Desktop/smart research assistant/retriever.py
@@ -3,3 +3,19 @@ import faiss
 import numpy as np
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
+def build_index(chunks):
+    embeddings = model.encode(chunks)
+    embeddings = np.array(embeddings).astype("float32")
+    dimension = embeddings.shape[1]
+    index = faiss.IndexFlatL2(dimension)
+    index.add(embeddings)
+    return index, embeddings
+
+def search(query, index, chunks, top_k=3):
+    query_embedding = model.encode([query])
+    query_embedding = np.array(query_embedding).astype("float32")
+    distances, indices = index.search(query_embedding, top_k)
+    results = []
+    for i in indices[0]:
+        results.append(chunks[1])
+    return results
