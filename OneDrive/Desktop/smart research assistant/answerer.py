de4ecc23eda6225735f95c8cd3b1be2a9b1c6969
Soaham Pahwa <soaham221@gmail.com>
2026-04-14 16:30:53 +0530

    API changed

--- OneDrive/Desktop/smart research assistant/answerer.py
+++ OneDrive/Desktop/smart research assistant/answerer.py
@@ -1,58 +1,38 @@
-import requests
+import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-HF_TOKEN = os.getenv("HF_TOKEN")
-API_URL = "https://router.huggingface.co/hf-inference/models/mistralai/Mistral-7B-Instruct-v0.3/v1/chat/completions"
-
-headers = {
-    "Authorization" : f"Bearer {HF_TOKEN}"
-}
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 def build_prompt(query, chunks):
     context = ""
     for i, chunk in enumerate(chunks):
         context += f"[Chunk {i+1}]:\n{chunk}\n\n"
 
-    prompt = f"""<s>[INST] You are a helpful research assistant.isinstance
-Use ONLY the following chunks from a document to answer the question.
-If the answer is not in the chunks, say exactly: "NOT_IN_DOCUMENT"
+    prompt = f"""You are a helful research assistant.
+Use ONLY the following chunks from a document to answer the question. If the answer is not in the chunks, say exactly: NOT_IN_DOCUMENT
 Do not make up any information.
-Document chunks: {context}
+Document chunks:
+{context}
 Question : {query}
-Answer clearly and concisely in 3-4 sentences. [/INST]"""
+Answer clearly and concisely in 3-4 sentences."""
     return prompt
 
 def ask_llm(prompt):
-    payload = {
-        "model": "mistralai/Mistral-7B-Instruct-v0.3",
-        "messages": [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        "max_tokens": 300,
-        "temperature": 0.3
-    }
+    try:
+        response =  model.generate_content(prompt)
+        return response.text.strip()
+    except Exception as e:
+        return f"Error: {str(e)}"
     
-    response = requests.post(API_URL, headers=headers, json=payload)
-    
-    if response.status_code != 200:
-        return f"API Error: {response.status_code} - {response.text}"
-    
-    result = response.json()
-    answer = result["choices"][0]["message"]["content"]
-    return answer.strip()
-
 def get_answer_with_citation(query, chunks):
-    prompt = build_prompt(query, chunks)
+    prompt = build_propmt(query,chunks)
     answer = ask_llm(prompt)
 
     if "NOT_IN_DOCUMENT" in answer:
         return None, None
-    best_chunk_index = 0
-    citation = f"Source : Document, chunk {best_chunk_index+1}"
+    
+    citation = f"Source: Document, Chunk 1"
     return answer, citation
-
